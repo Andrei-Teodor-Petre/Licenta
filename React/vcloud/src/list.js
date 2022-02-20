5fd409176e345c39fb96f3570b3ce818de5417f5
Andrei <andreiteodorpetre@gmail.com>
2022-02-20 22:49:28 +0200

    Implementing blueprintjs

--- React/vcloud/src/list.js
+++ React/vcloud/src/list.js
@@ -1,13 +1,21 @@
+import {Card, Elevation, Classes} from "@blueprintjs/core";
+import './list.css';
+
 function List(){
     return(
-      <div className="List">
-        <ul>
-          <li>List item 1</li>
-          <li>List item 2</li>
-        </ul>
-      </div>
+        <Card className={className(Classes.PRIMARY, Classes.DARK)} interactive={true} elevation={Elevation.TWO}>
+            <h5>We did it!</h5>
+            <p>managed to succesfully implement the frontend stack as a trial!</p>
+            <div className="List">
+                <ul>
+                    <li>List item 1</li>
+                    <li>List item 2</li>
+                </ul>
+            </div>
+        </Card>
+
     );
-  }
+}
 
 
   
