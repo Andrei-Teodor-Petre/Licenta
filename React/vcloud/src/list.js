a91b1b07eea4aad88ed2ea54bf9b6cd1975c1bd1
Andrei <andreiteodorpetre@gmail.com>
2022-02-20 22:15:21 +0200

    added first test module

---
+++ React/vcloud/src/list.js
@@ -0,0 +1,14 @@
+function List(){
+    return(
+      <div className="List">
+        <ul>
+          <li>List item 1</li>
+          <li>List item 2</li>
+        </ul>
+      </div>
+    );
+  }
+
+
+  
+export default List;
