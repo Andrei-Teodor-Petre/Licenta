3f6f6d90008049d0c78f10ddf1d33247213bebd3
Andrei <andreiteodorpetre@gmail.com>
2022-07-01 11:37:41 +0300

    Fixed tests.py

--- VSapp/backend/tests.py
+++ VSapp/backend/tests.py
@@ -8,17 +8,17 @@ from PIL import Image
 class TestThumbnailMethods(unittest.TestCase):
 
 
-	# def test_create_thumbnail(self):
-	# 	#on upload this will call the ffmpeg library function and create a thumbnail for the video
+	def test_create_thumbnail(self):
+		#on upload this will call the ffmpeg library function and create a thumbnail for the video
 
-	# 	video_addr = f""f"{BASE_PATH}/Videos/video-{str(1)}.mov"
-	# 	image_address = f"{BASE_PATH}/Thumbnails/thumbnail-{str(1)}.jpeg"
-	# 	subprocess.call(['ffmpeg', '-y', '-i', video_addr, '-ss', '00:00:00.000', '-vframes', '1', image_address])
-	# 	thumbnail = Image.open(image_address)
-	# 	self.assertEqual((f"/Thumbnails/thumbnail-{str(1)}.jpeg", thumbnail.width, thumbnail.height), utils.create_thumbnail(video_addr,1))
+		video_addr = f""f"{BASE_PATH}/Videos/video-{str(1)}.mov"
+		image_address = f"{BASE_PATH}/Thumbnails/thumbnail-{str(1)}.jpeg"
+		subprocess.call(['ffmpeg', '-y', '-i', video_addr, '-ss', '00:00:00.000', '-vframes', '1', image_address])
+		thumbnail = Image.open(image_address)
+		self.assertEqual((f"/Thumbnails/thumbnail-{str(1)}.jpeg", thumbnail.width, thumbnail.height), utils.create_thumbnail(video_addr,1))
 
-	def test_upper(self):
-		self.assertEqual('foo'.upper(), 'FOO')
+	# def test_upper(self):
+	# 	self.assertEqual('foo'.upper(), 'FOO')
 
 	# def test_isupper(self):
 	# 	self.assertTrue('FOO'.isupper())
