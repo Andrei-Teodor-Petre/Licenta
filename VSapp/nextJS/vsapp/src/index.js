2f1d91e10e635ac7dd1a84503630518162f8cef9
Andrei <andreiteodorpetre@gmail.com>
2022-08-28 17:25:43 +0300

    Latest test version

--- VSapp/nextJS/vsapp/src/index.js
+++ VSapp/nextJS/vsapp/src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 
 
 class Square extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state = { 
+			value: null,
+		};
+	}
+	
 	render() {
 	  return (
-		<button className="square">
-		  {/* TODO */}
+		<button className="square" onClick={() => {
+			console.log('clicked');
+			this.setState({value: 'X'})
+		}
+		}>
+		  {this.props.value}
 		</button>
 	  );
 	}
@@ -15,7 +27,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
 	renderSquare(i) {
-	  return <Square />;
+	  return <Square value={i}/>;
 	}
   
 	render() {
